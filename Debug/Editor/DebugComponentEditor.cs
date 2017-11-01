7c08dbd393c0ae43409a18563705be2449c88209
Craiel <craieldev@gmail.com>
2017-10-31 23:22:40 -0400

    removed non-generic code

--- Debug/Editor/DebugComponentEditor.cs
+++ Debug/Editor/DebugComponentEditor.cs
@@ -1,9 +1,7 @@
 ﻿namespace Assets.Scripts.Craiel.Essentials.Debug.Editor
 {
     using Essentials.Editor.UserInterface;
-    using Logic.Stats;
     using UnityEditor;
-    using UnityEngine;
 
     public abstract class DebugComponentEditor : Editor
     {
@@ -14,16 +12,6 @@
         {
             toggle = Layout.DrawSectionHeaderToggleWithSection(title, toggle);
             return toggle;
-        }
-
-        protected void DrawEntityStatInfo(EntityStatSet stats)
-        {
-            GUILayout.Label(string.Format("HP: {0}/{1}", stats.GetHealth(), stats.GetHealthMax()));
-
-            GUILayout.Label(string.Format("SPD: {0}", stats.GetSpeed()));
-            GUILayout.Label(string.Format("OFF: {0}", stats.GetOffense()));
-            GUILayout.Label(string.Format("DEF: {0}", stats.GetDefense()));
-            GUILayout.Label(string.Format("STA: {0}", stats.GetStamina()));
         }
     }
 }

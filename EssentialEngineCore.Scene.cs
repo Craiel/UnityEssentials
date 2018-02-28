7c2b65b6f2b72ab60d3acc339fbda562aa0fc9cf
craiel <craieldev@gmail.com>
2018-02-28 17:34:18 -0500

    adding default core engine behavior like scene transitioning

---
+++ EssentialEngineCore.Scene.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Assets.Scripts.Core.Scenes;
+using Assets.Scripts.Craiel.Essentials.Scene;
+
+namespace Assets.Scripts.Craiel.UnityEssentials
+{
+    public abstract partial class EssentialEngineCore<T, TSceneEnum>
+    {
+        private readonly IDictionary<TSceneEnum, Type> sceneImplementations = new Dictionary<TSceneEnum, Type>();
+
+        private readonly IDictionary<TSceneEnum, BaseScene> scenes = new Dictionary<TSceneEnum, BaseScene>();
+
+        private BaseScene activeScene;
+
+        // -------------------------------------------------------------------
+        // Public
+        // -------------------------------------------------------------------
+        public TSceneEnum? ActiveSceneType { get; private set; }
+
+        public TS GetScene<TS>()
+            where TS : BaseScene
+        {
+            return (TS)this.activeScene;
+        }
+
+        protected void RegisterSceneImplementation<T>(TSceneEnum type)
+            where T : BaseScene
+        {
+            sceneImplementations.Add(type, typeof(T));
+        }
+
+        // -------------------------------------------------------------------
+        // Private
+        // -------------------------------------------------------------------
+        private void DestroyScene(TSceneEnum type)
+        {
+            if (!this.scenes.ContainsKey(type))
+            {
+                Logger.Warn("Scene {0} is not loaded, skipping shutdown", type);
+                return;
+            }
+
+            this.scenes.Remove(type);
+        }
+
+        private void LoadScene(TSceneEnum type)
+        {
+            if (!this.scenes.ContainsKey(type))
+            {
+                BaseScene scene = this.InitializeScene(type);
+                if (scene == null)
+                {
+                    return;
+                }
+
+                this.scenes.Add(type, scene);
+            }
+        }
+
+        private BaseScene InitializeScene(TSceneEnum type)
+        {
+            if (this.scenes.ContainsKey(type))
+            {
+                Logger.Warn("Scene {0} is already loaded, skipping", type);
+                return null;
+            }
+
+            Type implementation;
+            if (!sceneImplementations.TryGetValue(type, out implementation))
+            {
+                Logger.Error("Scene {0} has no implementation defined!", type);
+                return null;
+            }
+
+            if (!typeof(BaseScene).IsAssignableFrom(implementation))
+            {
+                Logger.Error("Scene implementation {0} is not of type IGameScene!", implementation);
+                return null;
+            }
+
+            return (BaseScene)Activator.CreateInstance(implementation);
+        }
+    }
+}
---
+++ EssentialEngineCore.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using Assets.Scripts.Craiel.Audio;
+using Assets.Scripts.Craiel.Essentials;
+using Assets.Scripts.Craiel.Essentials.Enums;
+using Assets.Scripts.Craiel.Essentials.Event;
+using Assets.Scripts.Craiel.Essentials.I18N;
+using Assets.Scripts.Craiel.Essentials.Input;
+using Assets.Scripts.Craiel.Essentials.Resource;
+using Assets.Scripts.Craiel.Essentials.Scene;
+using Assets.Scripts.Craiel.GameData;
+using JetBrains.Annotations;
+using NLog;
+
+namespace Assets.Scripts.Craiel.UnityEssentials
+{
+    public abstract partial class EssentialEngineCore<T, TSceneEnum> : UnitySingletonBehavior<T>
+        where T : EssentialEngineCore<T, TSceneEnum>
+        where TSceneEnum: struct, IConvertible
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        // -------------------------------------------------------------------
+        // Public
+        // -------------------------------------------------------------------
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            // System components first
+            ConfigureNLog();
+
+            ResourceProvider.InstantiateAndInitialize();
+            BundleProvider.InstantiateAndInitialize();
+            ResourceStreamProvider.InstantiateAndInitialize();
+
+            // Do localization fairly early so everything can run through it
+            LocalizationSystem.InstantiateAndInitialize();
+
+            // Register the root
+            SceneObjectController.Instance.RegisterObjectAsRoot(SceneRootCategory.System, this.gameObject, true);
+
+            // LoadFromProto resources all components will need
+            this.LoadPermanentResources();
+
+            // First some main systems
+            GameEvents.InstantiateAndInitialize();
+            GameRuntimeData.InstantiateAndInitialize();
+            InputHandler.InstantiateAndInitialize();
+            AudioSystem.InstantiateAndInitialize();
+            AudioAreaSystem.InstantiateAndInitialize();
+
+            try
+            {
+                // Initialize Game specific components
+                this.InitializeGameComponents();
+
+                UnityEngine.Debug.LogWarning("Essential Engine.Initialize() complete");
+            }
+            catch (Exception e)
+            {
+                UnityEngine.Debug.LogError("Error in Initialize of Sub-systems: " + e);
+                throw;
+            }
+        }
+
+        [UsedImplicitly]
+        public virtual void Update()
+        {
+            if (this.transitioning)
+            {
+                this.UpdateSceneTransition();
+            }
+        }
+
+#if UNITY_EDITOR
+        public IDictionary<ResourceKey, long> GetHistory()
+        {
+            return ResourceProvider.Instance.GetHistory();
+        }
+#endif
+
+        // -------------------------------------------------------------------
+        // Protected
+        // -------------------------------------------------------------------
+        protected virtual void LoadPermanentResources()
+        {
+            // Todo: Add resources we will always need
+
+            ResourceProvider.Instance.LoadImmediate();
+        }
+
+        protected abstract void InitializeGameComponents();
+    }
+}

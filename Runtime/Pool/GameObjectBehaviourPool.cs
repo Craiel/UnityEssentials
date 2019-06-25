b2d5aee985659c316068288ba7b5b2b7fc4f9aca
Patrick Gustavel <craieldev@gmail.com>
2019-06-25 16:18:42 -0400

    rename pool to reflect implementation

--- Runtime/Pool/GameObjectBehaviourPool.cs
+++ Runtime/Pool/GameObjectBehaviourPool.cs
@@ -2,20 +2,27 @@
 {
     using System;
     using Contracts;
+    using Resource;
+    using UnityEngine;
 
-    public abstract class TrackedPool<T> : BasePool<T>
-        where T : class, IPoolable
+    public class GameObjectBehaviourPool<T> : BasePool<T>
+        where T : MonoBehaviour, IPoolable
     {
         private const int DefaultSize = 20;
-
+        
         private T[] activeEntries;
 
         private int nextFreeSlot;
-
+        
+        private GameObject prefab;
+        private Func<T, bool> activeUpdateCallback;
+        
+        private Transform root;
+        
         // -------------------------------------------------------------------
         // Constructor
         // -------------------------------------------------------------------
-        protected TrackedPool(int capacity = DefaultSize)
+        protected GameObjectBehaviourPool(int capacity = DefaultSize)
             : base(initialCapacity: capacity)
         {
             this.activeEntries = new T[capacity];
@@ -25,7 +32,31 @@
         // Public
         // -------------------------------------------------------------------
         public int ActiveCount { get; private set; }
+        
+        private void Initialize(Func<T, bool> updateCallback, Transform poolRoot)
+        {
+            this.root = poolRoot;
+            this.activeUpdateCallback = updateCallback;
+        }
+        
+        public void Initialize(ResourceKey poolPrefabKey, Func<T, bool> updateCallback, Transform poolRoot = null)
+        {
+            this.Initialize(updateCallback, poolRoot);
 
+            this.prefab = poolPrefabKey.LoadManaged<GameObject>();
+            if (this.prefab == null)
+            {
+                throw new InvalidOperationException("Resource could not be loaded: " + poolPrefabKey);
+            }
+        }
+        
+        public void Initialize(GameObject poolPrefab, Func<T, bool> updateCallback, Transform poolRoot = null)
+        {
+            this.Initialize(updateCallback, poolRoot);
+
+            this.prefab = poolPrefab;
+        }
+        
         public void Update()
         {
             for (var i = 0; i < this.activeEntries.Length; i++)
@@ -48,7 +79,7 @@
                 }
             }
         }
-
+        
         public override T Obtain()
         {
             T entry = base.Obtain();
@@ -62,6 +93,18 @@
         // -------------------------------------------------------------------
         // Protected
         // -------------------------------------------------------------------
+        protected override T NewObject()
+        {
+            GameObject instance = UnityEngine.Object.Instantiate(this.prefab);
+
+            if (this.root != null)
+            {
+                instance.transform.SetParent(this.root);
+            }
+
+            return instance.GetComponentInChildren<T>();
+        }
+        
         protected override void Reset(T entry)
         {
             for (var i = 0; i < this.activeEntries.Length; i++)
@@ -83,8 +126,11 @@
             base.Reset(entry);
         }
 
-        protected abstract bool UpdateActiveEntry(T entry);
-
+        protected bool UpdateActiveEntry(T entry)
+        {
+            return this.activeUpdateCallback(entry);
+        }
+        
         // -------------------------------------------------------------------
         // Private
         // -------------------------------------------------------------------
